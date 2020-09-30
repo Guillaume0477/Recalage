6a842c5c39ba6d02fd5b65512594b3062b1b0283
Ant89ne <antoine.bralet@cpe.fr>
2020-09-30 09:36:08 +0200

    Exo2 q2

--- HelloWorld.cxx
+++ HelloWorld.cxx
@@ -1,10 +1,17 @@
 #include "itkImage.h"
+#include <itkImageFileReader.h> 
 #include <iostream>
 int main()
 {
     // Creer une image 3D avec un type de pixel unsigned short
     typedef itk::Image< unsigned short, 3 > ImageType;
     ImageType::Pointer image = ImageType::New();
-    std::cout << "ITK Hello World !" << std::endl;
+
+    typedef itk::ImageFileReader< ImageType > ReaderType;
+    ReaderType::Pointer reader = ReaderType::New();
+    reader->SetFileName( ../data/lena.jpg” );
+    reader->Update();
+    image = reader->GetOutput();
+
     return 0;
 }
