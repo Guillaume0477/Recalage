7a1e434ea8cbf88966cefc1af70021e19d8b35f7
Guillaume0477 <guillaumeduret1@gmail.com>
2020-09-30 10:03:35 +0200

    attempt file writer

--- HelloWorld.cxx
+++ HelloWorld.cxx
@@ -1,5 +1,6 @@
 #include "itkImage.h"
 #include <itkImageFileReader.h> 
+#include "itkImageFileWriter.h"
 #include <iostream>
 int main()
 {
@@ -11,13 +12,21 @@ int main()
     ReaderType::Pointer reader = ReaderType::New();
     reader->SetFileName( "../data/lena.jpg" );
     reader->Update();
-    image = reader->GetOutput();
+    //image = reader->GetOutput();
 
     ImageType::IndexType index;
     index[0] = 10;
     index[1] = 10;
 
     std::cout <<  (float) image->GetPixel(index) << std::endl;
+
+
+    typedef itk::ImageFileWriter<ImageType> WriterType ;
+
+    WriterType::Pointer writer = WriterType::New();
+    writer->SetFileName("new_image.png");
+    writer->SetInput(reader->GetOutput());
+    writer->Update();
 
     return 0;
 }
