9cb3c33d6c6e717a9303f0405d4d2ad6373b1f8c
Ant89ne <antoine.bralet@cpe.fr>
2020-09-30 09:59:04 +0200

    Gaussian filter

--- HelloWorld.cxx
+++ HelloWorld.cxx
@@ -1,11 +1,15 @@
 #include "itkImage.h"
 #include <itkImageFileReader.h> 
+#include <itkRecursiveGaussianImageFilter.h>
 #include <iostream>
 int main()
 {
+    float sigma = 15;
+
     // Creer une image 3D avec un type de pixel unsigned short
     typedef itk::Image< unsigned short, 2 > ImageType;
     ImageType::Pointer image = ImageType::New();
+    ImageType::Pointer imageFilt = ImageType::New();
 
     typedef itk::ImageFileReader< ImageType > ReaderType;
     ReaderType::Pointer reader = ReaderType::New();
@@ -13,11 +17,23 @@ int main()
     reader->Update();
     image = reader->GetOutput();
 
-    ImageType::IndexType index;
-    index[0] = 10;
-    index[1] = 10;
 
-    std::cout <<  (float) image->GetPixel(index) << std::endl;
+    //Read the [10,10] pixel
+    // ImageType::IndexType index;
+    // index[0] = 10;
+    // index[1] = 10;
+
+    // std::cout <<  (float) image->GetPixel(index) << std::endl;
+
+    typedef itk::RecursiveGaussianImageFilter< ImageType> FilterType;
+    FilterType::Pointer GaussFilt = FilterType::New();
+    GaussFilt->SetInput(image);
+    GaussFilt->SetSigma(sigma);
+    GaussFilt->Update();
+
+    imageFilt = GaussFilt->GetOutput();
+    
+
 
     return 0;
 }
