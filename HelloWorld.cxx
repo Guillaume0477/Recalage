50dcc5be43ba1a5211555ce98870a234a65ba01a
Ant89ne <antoine.bralet@cpe.fr>
2020-09-30 10:43:17 +0200

    .

--- HelloWorld.cxx
+++ HelloWorld.cxx
@@ -4,19 +4,20 @@
 #include <itkRecursiveGaussianImageFilter.h>
 #include <iostream>
 int main()
-{
-    float sigma = 1;
+{   
+    //Exercice 1
+    // float sigma = 1;
 
-    // Creer une image 2D avec un type de pixel unsigned char
-    typedef itk::Image< unsigned char, 2 > ImageType;
-    ImageType::Pointer image = ImageType::New();
-    ImageType::Pointer imageFilt = ImageType::New();
+    // // Creer une image 2D avec un type de pixel unsigned char
+    // typedef itk::Image< unsigned char, 2 > ImageType;
+    // ImageType::Pointer image = ImageType::New();
+    // ImageType::Pointer imageFilt = ImageType::New();
 
-    typedef itk::ImageFileReader< ImageType > ReaderType;
-    ReaderType::Pointer reader = ReaderType::New();
-    reader->SetFileName( "../data/lena.jpg" );
-    reader->Update();
-    image = reader->GetOutput();
+    // typedef itk::ImageFileReader< ImageType > ReaderType;
+    // ReaderType::Pointer reader = ReaderType::New();
+    // reader->SetFileName( "../data/lena.jpg" );
+    // reader->Update();
+    // image = reader->GetOutput();
 
 
     //Read the [10,10] pixel
@@ -36,6 +37,37 @@ int main()
     // imageFilt = GaussFilt->GetOutput();
     
     //Exercice 5
+
+    //TODO lire Image Moving, image Fix
+
+
+    typedef itk::ImageRegistrationMethod<ImageType,ImageType> RegistrationType;
+    registration->SetOptimizer(optimizer);
+    registration->SetTransform(transform);
+    registration->SetInterpolator( interpolator );
+    registration->SetMetric( metric );
+    registration->SetFixedImage( fixedImageReader->GetOutput() );
+    registration->SetMovingImage( movingImage );
+    registration->SetFixedImageRegion(
+    fixedImageReader->GetOutput()->GetBufferedRegion() );
+
+    typedef RegistrationType::ParametersType ParametersType;
+    ParametersType initialParameters( transform->GetNumberOfParameters() );
+    initialParameters[0] = 0.0;
+    initialParameters[1] = 0.0;
+    // Initial offset in mm along X
+    // Initial offset in mm along Y
+    registration->SetInitialTransformParameters( initialParameters );
+    try
+    {
+        registration->StartRegistration();
+    }
+        catch( itk::ExceptionObject & err )
+    {
+        std::cout << "ExceptionObject caught !" << std::endl;
+        std::cout << err << std::endl;
+        return -1;
+    }
 
     //Exercice 3
     // typedef itk::ImageFileWriter<ImageType> WriterType ;
