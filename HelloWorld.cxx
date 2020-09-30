14084c8b746e7556cf38430f2d21e02cd26d918d
Ant89ne <antoine.bralet@cpe.fr>
2020-09-30 09:43:33 +0200

    Exo2 OK

--- HelloWorld.cxx
+++ HelloWorld.cxx
@@ -4,14 +4,20 @@
 int main()
 {
     // Creer une image 3D avec un type de pixel unsigned short
-    typedef itk::Image< unsigned short, 3 > ImageType;
+    typedef itk::Image< unsigned short, 2 > ImageType;
     ImageType::Pointer image = ImageType::New();
 
     typedef itk::ImageFileReader< ImageType > ReaderType;
     ReaderType::Pointer reader = ReaderType::New();
-    reader->SetFileName( ../data/lena.jpg” );
+    reader->SetFileName( "../data/lena.jpg" );
     reader->Update();
     image = reader->GetOutput();
+
+    ImageType::IndexType index;
+    index[0] = 10;
+    index[1] = 10;
+
+    std::cout <<  (float) image->GetPixel(index) << std::endl;
 
     return 0;
 }
